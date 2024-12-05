ae4002be8e0ec4e40ba7e2e9ad537484e3535a39
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-05 10:21:19 +0800

    Update fpn.py

--- unlanedet/model/module/neck/fpn.py
+++ unlanedet/model/module/neck/fpn.py
@@ -10,6 +10,79 @@ import torch.nn.functional as F
 from mmcv.cnn import ConvModule
 from ....layers import Conv2d,get_norm,Activation
 
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class ConvBNReLU(nn.Module):
+    '''Module for the Conv-BN-ReLU tuple.'''
+    def __init__(self, c_in, c_out, kernel_size, stride, padding, dilation,
+                 use_relu=True):
+        super(ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(
+                c_in, c_out, kernel_size=kernel_size, stride=stride, 
+                padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(c_out)
+        if use_relu:
+            self.relu = nn.ReLU(inplace=True)
+        else:
+            self.relu = None
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class CARAFE(nn.Module):
+    def __init__(self, c, c_mid=64, scale=2, k_up=5, k_enc=3):
+        """ The unofficial implementation of the CARAFE module.
+
+        The details are in "https://arxiv.org/abs/1905.02188".
+
+        Args:
+            c: The channel number of the input and the output.
+            c_mid: The channel number after compression.
+            scale: The expected upsample scale.
+            k_up: The size of the reassembly kernel.
+            k_enc: The kernel size of the encoder.
+
+        Returns:
+            X: The upsampled feature map.
+        """
+        super(CARAFE, self).__init__()
+        self.scale = scale
+
+        self.comp = ConvBNReLU(c, c_mid, kernel_size=1, stride=1, 
+                               padding=0, dilation=1)
+        self.enc = ConvBNReLU(c_mid, (scale*k_up)**2, kernel_size=k_enc, 
+                              stride=1, padding=k_enc//2, dilation=1, 
+                              use_relu=False)
+        self.pix_shf = nn.PixelShuffle(scale)
+
+        self.upsmp = nn.Upsample(scale_factor=scale, mode='nearest')
+        self.unfold = nn.Unfold(kernel_size=k_up, dilation=scale, 
+                                padding=k_up//2*scale)
+
+    def forward(self, X):
+        b, c, h, w = X.size()
+        h_, w_ = h * self.scale, w * self.scale
+        
+        W = self.comp(X)                                # b * m * h * w
+        W = self.enc(W)                                 # b * 100 * h * w
+        W = self.pix_shf(W)                             # b * 25 * h_ * w_
+        W = F.softmax(W, dim=1)                         # b * 25 * h_ * w_
+
+        X = self.upsmp(X)                               # b * c * h_ * w_
+        X = self.unfold(X)                              # b * 25c * h_ * w_
+        X = X.view(b, c, -1, h_, w_)                    # b * 25 * c * h_ * w_
+
+        X = torch.einsum('bkhw,bckhw->bchw', [W, X])    # b * c * h_ * w_
+        return X
+
 class FPN(nn.Module):
     def __init__(self,
                  in_channels,
@@ -32,39 +105,46 @@ class FPN(nn.Module):
                  cfg=None):
         super(FPN, self).__init__()
         # assert isinstance(in_channels, list)
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.num_ins = len(in_channels)
-        self.num_outs = num_outs
+        self.in_channels = in_channels  # 保存输入通道列表
+        self.out_channels = out_channels  # 保存输出通道数
+        self.num_ins = len(in_channels)  # 输入的特征图数量（等于 in_channels 的长度）
+        self.num_outs = num_outs  # 输出特征图的层数
 
-        self.backbone_end_level = self.num_ins
-        self.start_level = 0
-        self.lateral_convs = nn.ModuleList()
-        self.fpn_convs = nn.ModuleList()
+        self.backbone_end_level = self.num_ins  # 设置骨干网络的结束层（即输入的层数）
+        self.start_level = 0  # 设置起始层，通常为 0
+        self.lateral_convs = nn.ModuleList()  # 用于存储 lateral 卷积层的列表
+        self.fpn_convs = nn.ModuleList()  # 用于存储 FPN 卷积层的列表
+        self.upsamples = nn.ModuleList()
 
+        # 初始化 lateral 卷积和 FPN 卷积层
         for i in range(self.start_level, self.backbone_end_level):
+            #横向卷积层,1*1卷积用于保持通道统一
             l_conv = ConvModule(
-                in_channels[i],
-                out_channels,
-                1,
-                conv_cfg=None,
-                norm_cfg=None,
-                act_cfg=None,
+                in_channels[i],  # 输入通道数
+                out_channels,  # 输出通道数
+                1,  # 卷积核大小为 1
+                conv_cfg=None,  # 卷积配置（未指定）
+                norm_cfg=None,  # 归一化配置（未指定）
+                act_cfg=None,  # 激活函数配置（未指定）
                 inplace=False,
             )
+            # FPN 卷积层，处理后的通道和尺寸都不变
             fpn_conv = ConvModule(
-                out_channels,
-                out_channels,
-                3,
-                padding=1,
-                conv_cfg=None,
-                norm_cfg=None,
-                act_cfg=None,
+                out_channels,  # 输入通道数为输出通道数
+                out_channels,  # 输出通道数
+                3,  # 卷积核大小为 3
+                padding=1,  # padding 为 1
+                conv_cfg=None,  # 卷积配置（未指定）
+                norm_cfg=None,  # 归一化配置（未指定）
+                act_cfg=None,  # 激活函数配置（未指定）
                 inplace=False,
             )
 
-            self.lateral_convs.append(l_conv)
-            self.fpn_convs.append(fpn_conv)
+            self.lateral_convs.append(l_conv)  # 将 lateral 卷积层添加到列表中
+            self.fpn_convs.append(fpn_conv)  # 将 FPN 卷积层添加到列表中
+        
+        for i in range(len(self.lateral_convs)-1):
+            self.upsamples.append(CARAFE(out_channels))
 
     def forward(self, inputs):
         """
@@ -79,28 +159,29 @@ class FPN(nn.Module):
               Example of shapes:
                 ([1, 64, 40, 100], [1, 64, 20, 50], [1, 64, 10, 25]).
         """
-        if type(inputs) == tuple:
+        if type(inputs) == tuple:  # 如果输入是 tuple 类型，将其转换为 list 类型
             inputs = list(inputs)
 
-        assert len(inputs) >= len(self.in_channels)  # 4 > 3
+        assert len(inputs) >= len(self.in_channels)  # 确保输入的特征图数量不小于 in_channels 的长度
 
-        if len(inputs) > len(self.in_channels):
-            for _ in range(len(inputs) - len(self.in_channels)):
+        if len(inputs) > len(self.in_channels):  # 如果输入的特征图数量大于 in_channels 的长度
+            for _ in range(len(inputs) - len(self.in_channels)):  # 删除多余的输入特征图
                 del inputs[0]
 
-        # build laterals
+        # 构建 lateral 卷积层的输出
         laterals = [
-            lateral_conv(inputs[i + self.start_level])
+            lateral_conv(inputs[i + self.start_level])  # 通过 lateral 卷积对每个输入特征图进行处理
             for i, lateral_conv in enumerate(self.lateral_convs)
         ]
 
-        # build top-down path
-        used_backbone_levels = len(laterals)
-        for i in range(used_backbone_levels - 1, 0, -1):
-            prev_shape = laterals[i - 1].shape[2:]
-            laterals[i - 1] += F.interpolate(
-                laterals[i], size=prev_shape, mode='nearest'
-            )
+        # 构建自顶向下的路径
+        used_backbone_levels = len(laterals)  # 使用的骨干网络层数，即 lateral 卷积层的数量
+        for i in range(used_backbone_levels - 1, 0, -1):  # 从最后一层往前遍历
+            laterals[i - 1] += self.upsamples[i-1](laterals[i])  # 将当前层的特征图上采样到上一层的大小并进行融合
 
+
+        # 对每一层的特征图进行 FPN 卷积处理
         outs = [self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)]
+        
+        # 返回每一层的输出特征图
         return tuple(outs)
