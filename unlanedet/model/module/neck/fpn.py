82f5c2961840d3a5b3bc2c8ab8156ccce71b3bf6
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-09 11:14:37 +0800

    Update fpn.py

--- unlanedet/model/module/neck/fpn.py
+++ unlanedet/model/module/neck/fpn.py
@@ -34,78 +34,53 @@ class FPN(nn.Module):
         self.out_channels = out_channels
         self.num_ins = len(in_channels)
         self.num_outs = num_outs
-        self.attention = attention
-        self.relu_before_extra_convs = relu_before_extra_convs
-        self.no_norm_on_lateral = no_norm_on_lateral
-        self.upsample_cfg = upsample_cfg.copy()
 
-        if end_level == -1:
-            self.backbone_end_level = self.num_ins
-            assert num_outs >= self.num_ins - start_level
-        else:
-            # if end_level < inputs, no extra level is allowed
-            self.backbone_end_level = end_level
-            assert end_level <= len(in_channels)
-            assert num_outs == end_level - start_level
-        self.start_level = start_level
-        self.end_level = end_level
-        self.add_extra_convs = add_extra_convs
-        assert isinstance(add_extra_convs, (str, bool))
-        if isinstance(add_extra_convs, str):
-            # Extra_convs_source choices: 'on_input', 'on_lateral', 'on_output'
-            assert add_extra_convs in ('on_input', 'on_lateral', 'on_output')
-        elif add_extra_convs:  # True
-            if extra_convs_on_inputs:
-                # TODO: deprecate `extra_convs_on_inputs`
-                warnings.simplefilter('once')
-                warnings.warn(
-                    '"extra_convs_on_inputs" will be deprecated in v2.9.0,'
-                    'Please use "add_extra_convs"', DeprecationWarning)
-                self.add_extra_convs = 'on_input'
-            else:
-                self.add_extra_convs = 'on_output'
-
+        self.backbone_end_level = self.num_ins
+        self.start_level = 0
         self.lateral_convs = nn.ModuleList()
         self.fpn_convs = nn.ModuleList()
 
+        # Learnable weights for Fast Normalized Fusion
+        self.fusion_weights = nn.Parameter(torch.ones(2*(self.num_ins-1), requires_grad=True))
+
         for i in range(self.start_level, self.backbone_end_level):
-            l_conv = Conv2d(
+            l_conv = ConvModule(
                 in_channels[i],
                 out_channels,
                 1,
-                norm = get_norm(norm_cfg,out_channels) if not self.no_norm_on_lateral else None,
-                activation = Activation(act=act_cfg)
+                conv_cfg=None,
+                norm_cfg=None,
+                act_cfg=None,
+                inplace=False,
             )
-            fpn_conv = Conv2d(out_channels,
-                              out_channels,
-                              3,
-                              padding=1,
-                              norm = get_norm(norm_cfg,out_channels),
-                              activation = Activation(act=act_cfg))
+            fpn_conv = ConvModule(
+                out_channels,
+                out_channels,
+                3,
+                padding=1,
+                conv_cfg=None,
+                norm_cfg=None,
+                act_cfg=None,
+                inplace=False,
+            )
 
             self.lateral_convs.append(l_conv)
             self.fpn_convs.append(fpn_conv)
 
-        # add extra conv layers (e.g., RetinaNet)
-        extra_levels = num_outs - self.backbone_end_level + self.start_level
-        if self.add_extra_convs and extra_levels >= 1:
-            for i in range(extra_levels):
-                if i == 0 and self.add_extra_convs == 'on_input':
-                    in_channels = self.in_channels[self.backbone_end_level - 1]
-                else:
-                    in_channels = out_channels
-                extra_fpn_conv = Conv2d(in_channels,
-                                        out_channels,
-                                        3,
-                                        stride=2,
-                                        padding=1,
-                                        norm = get_norm(norm_cfg,out_channels),
-                                        activation = Activation(act=act_cfg)
-                                         )
-                self.fpn_convs.append(extra_fpn_conv)
-
     def forward(self, inputs):
-        """Forward function."""
+        """
+        Args:
+            inputs (List[torch.Tensor]): Input feature maps.
+              Example of shapes:
+                ([1, 64, 80, 200], [1, 128, 40, 100], [1, 256, 20, 50], [1, 512, 10, 25]).
+        Returns:
+            outputs (Tuple[torch.Tensor]): Output feature maps.
+              Example of shapes:
+                ([1, 64, 40, 100], [1, 64, 20, 50], [1, 64, 10, 25]).
+        """
+        if type(inputs) == tuple:
+            inputs = list(inputs)
+
         assert len(inputs) >= len(self.in_channels)
 
         if len(inputs) > len(self.in_channels):
@@ -118,46 +93,23 @@ class FPN(nn.Module):
             for i, lateral_conv in enumerate(self.lateral_convs)
         ]
 
-        # build top-down path
+        # Normalize weights for Fast Normalized Fusion
+        fusion_weights = F.relu(self.fusion_weights)  # Ensure non-negative
+
+        # build top-down path with Fast Normalized Fusion
         used_backbone_levels = len(laterals)
         for i in range(used_backbone_levels - 1, 0, -1):
-            # In some cases, fixing `scale factor` (e.g. 2) is preferred, but
-            #  it cannot co-exist with `size` in `F.interpolate`.
-            if 'scale_factor' in self.upsample_cfg:
-                laterals[i - 1] += F.interpolate(laterals[i],
-                                                 **self.upsample_cfg)
-            else:
-                prev_shape = laterals[i - 1].shape[2:]
-                laterals[i - 1] += F.interpolate(laterals[i],
-                                                 size=prev_shape,
-                                                 **self.upsample_cfg)
+            prev_shape = laterals[i - 1].shape[2:]
+            upsampled = F.interpolate(
+                laterals[i], size=prev_shape, mode='nearest'
+            )
+            division = fusion_weights[(i - 1)*2]+fusion_weights[(i - 1)*2+1]+1e-6
+            # Apply normalized weights
+            laterals[i - 1] = (
+                fusion_weights[(i - 1)*2] /division* laterals[i - 1]
+                + fusion_weights[(i - 1)*2+1] /division* upsampled
+            )
 
-        # build outputs
-        # part 1: from original levels
-        outs = [
-            self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)
-        ]
-        # part 2: add extra levels
-        if self.num_outs > len(outs):
-            # use max pool to get more levels on top of outputs
-            # (e.g., Faster R-CNN, Mask R-CNN)
-            if not self.add_extra_convs:
-                for i in range(self.num_outs - used_backbone_levels):
-                    outs.append(F.max_pool2d(outs[-1], 1, stride=2))
-            # add conv layers on top of original feature maps (RetinaNet)
-            else:
-                if self.add_extra_convs == 'on_input':
-                    extra_source = inputs[self.backbone_end_level - 1]
-                elif self.add_extra_convs == 'on_lateral':
-                    extra_source = laterals[-1]
-                elif self.add_extra_convs == 'on_output':
-                    extra_source = outs[-1]
-                else:
-                    raise NotImplementedError
-                outs.append(self.fpn_convs[used_backbone_levels](extra_source))
-                for i in range(used_backbone_levels + 1, self.num_outs):
-                    if self.relu_before_extra_convs:
-                        outs.append(self.fpn_convs[i](F.relu(outs[-1])))
-                    else:
-                        outs.append(self.fpn_convs[i](outs[-1]))
+        # Apply fpn_convs to each lateral
+        outs = [self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)]
         return tuple(outs)
