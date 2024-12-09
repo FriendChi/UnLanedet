eb658c16c6963aae36ee136ddd9e0d3643a1ddac
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-09 10:00:02 +0800

    Update fpn.py

--- unlanedet/model/module/neck/fpn.py
+++ unlanedet/model/module/neck/fpn.py
@@ -48,8 +48,6 @@ class LGAG(nn.Module):
             nn.Sigmoid()
         )
         self.activation = act_layer(activation, inplace=True)
-
-        self.init_weights('normal')
     
                 
     def forward(self, g, x):
