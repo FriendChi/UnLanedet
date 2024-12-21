1dd9c180ba5c339d04b6e91c5be8b85c718242b5
FriendChi <137796057+FriendChi@users.noreply.github.com>
2024-12-21 12:17:10 +0800

    Update fpn.py

--- unlanedet/model/module/neck/fpn.py
+++ unlanedet/model/module/neck/fpn.py
@@ -9,54 +9,345 @@ import torch.nn.functional as F
 
 from mmcv.cnn import ConvModule
 from ....layers import Conv2d,get_norm,Activation
-def act_layer(act, inplace=False, neg_slope=0.2, n_prelu=1):
-    # activation layer
-    act = act.lower()
-    if act == 'relu':
-        layer = nn.ReLU(inplace)
-    elif act == 'relu6':
-        layer = nn.ReLU6(inplace)
-    elif act == 'leakyrelu':
-        layer = nn.LeakyReLU(neg_slope, inplace)
-    elif act == 'prelu':
-        layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
-    elif act == 'gelu':
-        layer = nn.GELU()
-    elif act == 'hswish':
-        layer = nn.Hardswish(inplace)
-    else:
-        raise NotImplementedError('activation layer [%s] is not found' % act)
-    return layer
 
-class LGAG(nn.Module):
-    def __init__(self, F_g, F_l, F_int, kernel_size=3, groups=1, activation='relu'):
-        super(LGAG,self).__init__()
+class DSConv_pro(nn.Module):
+    def __init__(
+        self,
+        in_channels: int = 1,
+        out_channels: int = 1,
+        kernel_size: int = 9,
+        extend_scope: float = 1.0,
+        morph: int = 0,
+        if_offset: bool = True,
+        device: str | torch.device = "cuda",
+    ):
+        """    
+        基于：
 
-        if kernel_size == 1:
-            groups = 1
-        self.W_g = nn.Sequential(
-            nn.Conv2d(F_g, F_int, kernel_size=kernel_size, stride=1, padding=kernel_size//2, groups=groups, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-        self.W_x = nn.Sequential(
-            nn.Conv2d(F_l, F_int, kernel_size=kernel_size, stride=1, padding=kernel_size//2, groups=groups, bias=True),
-            nn.BatchNorm2d(F_int)
-        )
-        self.psi = nn.Sequential(
-            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
-        )
-        self.activation = act_layer(activation, inplace=True)
-    
-                
-    def forward(self, g, x):
-        g1 = self.W_g(g)
-        x1 = self.W_x(x)
-        psi = self.activation(g1 + x1)
-        psi = self.psi(psi)
+            TODO
 
-        return x*psi
+        参数：
+            in_ch: 输入通道数。默认为1。
+            out_ch: 输出通道数。默认为1。
+            kernel_size: 卷积核的大小。默认为9。
+            extend_scope: 扩展的范围。此方法默认为1。
+            morph: 卷积核的形态主要沿x轴（0）和y轴（1）分为两种类型（详见论文）。
+            if_offset: 是否需要变形，如果为False，则是标准卷积核。默认为True。
+
+        """
+        """
+        A Dynamic Snake Convolution Implementation
+
+        Based on:
+
+            TODO
+
+        Args:
+            in_ch: number of input channels. Defaults to 1.
+            out_ch: number of output channels. Defaults to 1.
+            kernel_size: the size of kernel. Defaults to 9.
+            extend_scope: the range to expand. Defaults to 1 for this method.
+            morph: the morphology of the convolution kernel is mainly divided into two types along the x-axis (0) and the y-axis (1) (see the paper for details).
+            if_offset: whether deformation is required,  if it is False, it is the standard convolution kernel. Defaults to True.
+
+        """
+
+        super().__init__()
+
+        if morph not in (0, 1):
+            raise ValueError("morph should be 0 or 1.")
+
+        self.kernel_size = kernel_size
+        self.extend_scope = extend_scope
+        self.morph = morph
+        self.if_offset = if_offset
+        self.device = torch.device(device)
+        self.to(device)
+
+        # self.bn = nn.BatchNorm2d(2 * kernel_size)
+        self.gn_offset = nn.GroupNorm(kernel_size, 2 * kernel_size)
+        self.gn = nn.GroupNorm(out_channels // 4, out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.tanh = nn.Tanh()
+
+        self.offset_conv = nn.Conv2d(in_channels, 2 * kernel_size, 3, padding=1)
+
+        self.dsc_conv_x = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=(kernel_size, 1),
+            stride=(kernel_size, 1),
+            padding=0,
+        )
+        self.dsc_conv_y = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=(1, kernel_size),
+            stride=(1, kernel_size),
+            padding=0,
+        )
+
+    def forward(self, input: torch.Tensor):
+        # 预测偏移量映射，值域为[-1, 1]
+        # Predict offset map between [-1, 1]
+        offset = self.offset_conv(input)
+        # 注释掉的批归一化操作
+        # offset = self.bn(offset)
+        # 使用组归一化对偏移量进行归一化
+        offset = self.gn_offset(offset)
+        # 对偏移量进行双曲正切激活
+        offset = self.tanh(offset)
+
+        # 执行可变形卷积，得到x轴和y轴的坐标映射
+        # Run deformative conv
+        y_coordinate_map, x_coordinate_map = get_coordinate_map_2D(
+            offset=offset,
+            morph=self.morph,
+            extend_scope=self.extend_scope,
+            device=self.device,
+        )
+        # 根据坐标映射插值得到变形后的特征
+        deformed_feature = get_interpolated_feature(
+            input,
+            y_coordinate_map,
+            x_coordinate_map,
+        )
+
+        # 根据形态参数决定使用哪个卷积层进行卷积操作 ([1, 1, 48, 16])->([1, 16, 16, 16])
+        if self.morph == 0:
+            output = self.dsc_conv_x(deformed_feature)
+        elif self.morph == 1:
+            output = self.dsc_conv_y(deformed_feature)
+
+        # 对输出进行组归一化和ReLU激活
+        # Groupnorm & ReLU
+        output = self.gn(output)
+        output = self.relu(output)
+
+        return output
+
+
+def get_coordinate_map_2D(
+    offset: torch.Tensor,
+    morph: int,
+    extend_scope: float = 1.0,
+    device: str | torch.device = "cuda",
+):
+    """
+    基于给定的偏移量计算DSCNet的2D坐标映射。
+
+    Args:
+        offset (torch.Tensor): 网络预测的偏移量，形状为[B, 2*K, W, H]。其中B表示批量大小，K表示卷积核大小，W和H分别表示宽度和高度。
+        morph (int): 卷积核的形态，主要分为沿x轴(0)和沿y轴(1)两种类型（详见论文）。
+        extend_scope (float, optional): 展开的范围。默认值为1。
+        device (str | torch.device, optional): 数据所在位置。默认为'cuda'。
+
+    Returns:
+        tuple: 包含两个元素的元组，分别为：
+        - y_coordinate_map (torch.Tensor): 沿y轴的坐标映射，形状为[B, K_H * H, K_W * W]。
+        - x_coordinate_map (torch.Tensor): 沿x轴的坐标映射，形状为[B, K_H * H, K_W * W]。
+
+    Raises:
+        ValueError: 如果morph不是0或1时，会抛出ValueError异常。
+    """
+    """Computing 2D coordinate map of DSCNet based on: TODO
+
+    Args:
+        offset: offset predict by network with shape [B, 2*K, W, H]. Here K refers to kernel size.
+        morph: the morphology of the convolution kernel is mainly divided into two types along the x-axis (0) and the y-axis (1) (see the paper for details).
+        extend_scope: the range to expand. Defaults to 1 for this method.
+        device: location of data. Defaults to 'cuda'.
+
+    Return:
+        y_coordinate_map: coordinate map along y-axis with shape [B, K_H * H, K_W * W]
+        x_coordinate_map: coordinate map along x-axis with shape [B, K_H * H, K_W * W]
+    """
+
+    if morph not in (0, 1):
+        raise ValueError("morph should be 0 or 1.")
+
+    batch_size, _, width, height = offset.shape
+    kernel_size = offset.shape[1] // 2
+    center = kernel_size // 2
+    device = torch.device(device)
+
+    y_offset_, x_offset_ = torch.split(offset, kernel_size, dim=1)
+
+    y_center_ = torch.arange(0, width, dtype=torch.float32, device=device)
+    y_center_ = einops.repeat(y_center_, "w -> k w h", k=kernel_size, h=height)
+
+    x_center_ = torch.arange(0, height, dtype=torch.float32, device=device)
+    x_center_ = einops.repeat(x_center_, "h -> k w h", k=kernel_size, w=width)
+
+    if morph == 0:
+        """
+        Initialize the kernel and flatten the kernel
+            y: only need 0
+            x: -num_points//2 ~ num_points//2 (Determined by the kernel size)
+        """
+        y_spread_ = torch.zeros([kernel_size], device=device)
+        x_spread_ = torch.linspace(-center, center, kernel_size, device=device)
+
+        y_grid_ = einops.repeat(y_spread_, "k -> k w h", w=width, h=height)
+        x_grid_ = einops.repeat(x_spread_, "k -> k w h", w=width, h=height)
+
+        y_new_ = y_center_ + y_grid_
+        x_new_ = x_center_ + x_grid_
+
+        y_new_ = einops.repeat(y_new_, "k w h -> b k w h", b=batch_size)
+        x_new_ = einops.repeat(x_new_, "k w h -> b k w h", b=batch_size)
+
+        y_offset_ = einops.rearrange(y_offset_, "b k w h -> k b w h")
+        y_offset_new_ = y_offset_.detach().clone()
+
+        # The center position remains unchanged and the rest of the positions begin to swing
+        # This part is quite simple. The main idea is that "offset is an iterative process"
+
+        y_offset_new_[center] = 0
+
+        for index in range(1, center + 1):
+            y_offset_new_[center + index] = (
+                y_offset_new_[center + index - 1] + y_offset_[center + index]
+            )
+            y_offset_new_[center - index] = (
+                y_offset_new_[center - index + 1] + y_offset_[center - index]
+            )
+
+        y_offset_new_ = einops.rearrange(y_offset_new_, "k b w h -> b k w h")
+
+        y_new_ = y_new_.add(y_offset_new_.mul(extend_scope))
+
+        y_coordinate_map = einops.rearrange(y_new_, "b k w h -> b (w k) h")
+        x_coordinate_map = einops.rearrange(x_new_, "b k w h -> b (w k) h")
+
+    elif morph == 1:
+        """
+        Initialize the kernel and flatten the kernel
+            y: -num_points//2 ~ num_points//2 (Determined by the kernel size)
+            x: only need 0
+        """
+        y_spread_ = torch.linspace(-center, center, kernel_size, device=device)
+        x_spread_ = torch.zeros([kernel_size], device=device)
+
+        y_grid_ = einops.repeat(y_spread_, "k -> k w h", w=width, h=height)
+        x_grid_ = einops.repeat(x_spread_, "k -> k w h", w=width, h=height)
+
+        y_new_ = y_center_ + y_grid_
+        x_new_ = x_center_ + x_grid_
+
+        y_new_ = einops.repeat(y_new_, "k w h -> b k w h", b=batch_size)
+        x_new_ = einops.repeat(x_new_, "k w h -> b k w h", b=batch_size)
+
+        x_offset_ = einops.rearrange(x_offset_, "b k w h -> k b w h")
+        x_offset_new_ = x_offset_.detach().clone()
+
+        # The center position remains unchanged and the rest of the positions begin to swing
+        # This part is quite simple. The main idea is that "offset is an iterative process"
+
+        x_offset_new_[center] = 0
+
+        for index in range(1, center + 1):
+            x_offset_new_[center + index] = (
+                x_offset_new_[center + index - 1] + x_offset_[center + index]
+            )
+            x_offset_new_[center - index] = (
+                x_offset_new_[center - index + 1] + x_offset_[center - index]
+            )
+
+        x_offset_new_ = einops.rearrange(x_offset_new_, "k b w h -> b k w h")
+
+        x_new_ = x_new_.add(x_offset_new_.mul(extend_scope))
+
+        y_coordinate_map = einops.rearrange(y_new_, "b k w h -> b w (h k)")
+        x_coordinate_map = einops.rearrange(x_new_, "b k w h -> b w (h k)")
+
+    return y_coordinate_map, x_coordinate_map
+
+
+def get_interpolated_feature(
+    input_feature: torch.Tensor,
+    y_coordinate_map: torch.Tensor,
+    x_coordinate_map: torch.Tensor,
+    interpolate_mode: str = "bilinear",
+):
+    """From coordinate map interpolate feature of DSCNet based on: TODO
+
+    Args:
+        input_feature: feature that to be interpolated with shape [B, C, H, W]
+        y_coordinate_map: coordinate map along y-axis with shape [B, K_H * H, K_W * W]
+        x_coordinate_map: coordinate map along x-axis with shape [B, K_H * H, K_W * W]
+        interpolate_mode: the arg 'mode' of nn.functional.grid_sample, can be 'bilinear' or 'bicubic' . Defaults to 'bilinear'.
+
+    Return:
+        interpolated_feature: interpolated feature with shape [B, C, K_H * H, K_W * W]
+    """
+
+    if interpolate_mode not in ("bilinear", "bicubic"):
+        raise ValueError("interpolate_mode should be 'bilinear' or 'bicubic'.")
+
+    y_max = input_feature.shape[-2] - 1
+    x_max = input_feature.shape[-1] - 1
+
+    y_coordinate_map_ = _coordinate_map_scaling(y_coordinate_map, origin=[0, y_max])
+    x_coordinate_map_ = _coordinate_map_scaling(x_coordinate_map, origin=[0, x_max])
+
+    y_coordinate_map_ = torch.unsqueeze(y_coordinate_map_, dim=-1)
+    x_coordinate_map_ = torch.unsqueeze(x_coordinate_map_, dim=-1)
+
+    # Note here grid with shape [B, H, W, 2]
+    # Where [:, :, :, 2] refers to [x ,y]
+    grid = torch.cat([x_coordinate_map_, y_coordinate_map_], dim=-1)
+
+    interpolated_feature = nn.functional.grid_sample(
+        input=input_feature,
+        grid=grid,
+        mode=interpolate_mode,
+        padding_mode="zeros",
+        align_corners=True,
+    )
+
+    return interpolated_feature
+
+
+def _coordinate_map_scaling(
+    coordinate_map: torch.Tensor,
+    origin: list,
+    target: list = [-1, 1],
+):
+    """Map the value of coordinate_map from origin=[min, max] to target=[a,b] for DSCNet based on: TODO
+
+    Args:
+        coordinate_map: the coordinate map to be scaled
+        origin: original value range of coordinate map, e.g. [coordinate_map.min(), coordinate_map.max()]
+        target: target value range of coordinate map,Defaults to [-1, 1]
+
+    Return:
+        coordinate_map_scaled: the coordinate map after scaling
+    """
+    min, max = origin
+    a, b = target
+
+    coordinate_map_scaled = torch.clamp(coordinate_map, min, max)
+
+    scale_factor = (b - a) / (max - min)
+    coordinate_map_scaled = a + scale_factor * (coordinate_map_scaled - min)
+
+    return coordinate_map_scaled
+
+#只改变通道
+class EncoderConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(EncoderConv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+        self.gn = nn.GroupNorm(out_ch // 4, out_ch)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.gn(x)
+        x = self.relu(x)
+        return x
+
 class FPN(nn.Module):
     def __init__(self,
                  in_channels,
@@ -79,45 +370,57 @@ class FPN(nn.Module):
                  cfg=None):
         super(FPN, self).__init__()
         # assert isinstance(in_channels, list)
-        self.in_channels = in_channels  # 保存输入通道列表
-        self.out_channels = out_channels  # 保存输出通道数
-        self.num_ins = len(in_channels)  # 输入的特征图数量（等于 in_channels 的长度）
-        self.num_outs = num_outs  # 输出特征图的层数
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.num_ins = len(in_channels)
+        self.num_outs = num_outs
 
-        self.backbone_end_level = self.num_ins  # 设置骨干网络的结束层（即输入的层数）
-        self.start_level = 0  # 设置起始层，通常为 0
-        self.lateral_convs = nn.ModuleList()  # 用于存储 lateral 卷积层的列表
-        # self.fpn_convs = nn.ModuleList()  # 用于存储 FPN 卷积层的列表
-        self.galas = nn.ModuleList()
-        # 初始化 lateral 卷积和 FPN 卷积层
+        self.backbone_end_level = self.num_ins
+        self.start_level = 0
+        self.lateral_convs = nn.ModuleList()
+        self.fpn_convs = nn.ModuleList()
+        self.conv0x_list = nn.ModuleList()
+        self.conv0y_list = nn.ModuleList()
+        # Learnable weights for Fast Normalized Fusion
+        self.fusion_weights = nn.Parameter(torch.ones(2*(self.num_ins-1), requires_grad=True))
+
         for i in range(self.start_level, self.backbone_end_level):
-            #横向卷积层,1*1卷积用于保持通道统一
+            conv0x = DSConv_pro(
+                in_channels[i],
+                in_channels[i],
+                3,
+                0,
+            )
+            conv0y = DSConv_pro(
+                in_channels[i],
+                in_channels[i],
+                3,
+                1,
+            )
             l_conv = ConvModule(
-                in_channels[i],  # 输入通道数
-                out_channels,  # 输出通道数
-                1,  # 卷积核大小为 1
-                conv_cfg=None,  # 卷积配置（未指定）
-                norm_cfg=None,  # 归一化配置（未指定）
-                act_cfg=None,  # 激活函数配置（未指定）
+                2*in_channels[i],
+                out_channels,
+                1,
+                conv_cfg=None,
+                norm_cfg=None,
+                act_cfg=None,
                 inplace=False,
             )
-            # FPN 卷积层，处理后的通道和尺寸都不变
-            # fpn_conv = ConvModule(
-            #     out_channels,  # 输入通道数为输出通道数
-            #     out_channels,  # 输出通道数
-            #     3,  # 卷积核大小为 3
-            #     padding=1,  # padding 为 1
-            #     conv_cfg=None,  # 卷积配置（未指定）
-            #     norm_cfg=None,  # 归一化配置（未指定）
-            #     act_cfg=None,  # 激活函数配置（未指定）
-            #     inplace=False,
-            # )
+            fpn_conv = ConvModule(
+                out_channels,
+                out_channels,
+                3,
+                padding=1,
+                conv_cfg=None,
+                norm_cfg=None,
+                act_cfg=None,
+                inplace=False,
+            )
 
-            self.lateral_convs.append(l_conv)  # 将 lateral 卷积层添加到列表中
-            # self.fpn_convs.append(fpn_conv)  # 将 FPN 卷积层添加到列表中
-            
-        for i in range(len(self.lateral_convs)-1):
-            self.galas.append(LGAG(out_channels, out_channels, out_channels))
+            self.lateral_convs.append(l_conv)
+            self.fpn_convs.append(fpn_conv)
+            self.conv0x_list.append(conv0x)
+            self.conv0y_list.append(conv0y)
     def forward(self, inputs):
         """
         Args:
@@ -131,29 +434,32 @@ class FPN(nn.Module):
               Example of shapes:
                 ([1, 64, 40, 100], [1, 64, 20, 50], [1, 64, 10, 25]).
         """
-        if type(inputs) == tuple:  # 如果输入是 tuple 类型，将其转换为 list 类型
+        if type(inputs) == tuple:
             inputs = list(inputs)
 
-        assert len(inputs) >= len(self.in_channels)  # 确保输入的特征图数量不小于 in_channels 的长度
+        assert len(inputs) >= len(self.in_channels)  # 4 > 3
 
-        if len(inputs) > len(self.in_channels):  # 如果输入的特征图数量大于 in_channels 的长度
-            for _ in range(len(inputs) - len(self.in_channels)):  # 删除多余的输入特征图
+        if len(inputs) > len(self.in_channels):
+            for _ in range(len(inputs) - len(self.in_channels)):
                 del inputs[0]
 
-        # 构建 lateral 卷积层的输出
+        # build laterals
+         laterals = [
+            torch.cat((self.conv0x_list[i](inputs[i + self.start_level]), self.conv0y_list[i](inputs[i + self.start_level])), dim=1)
+            for i in range(self.lateral_convs)
+        ]       
         laterals = [
-            lateral_conv(inputs[i + self.start_level])  # 通过 lateral 卷积对每个输入特征图进行处理
+            lateral_conv(laterals[i])
             for i, lateral_conv in enumerate(self.lateral_convs)
         ]
 
-        # 构建自顶向下的路径
-        used_backbone_levels = len(laterals)  # 使用的骨干网络层数，即 lateral 卷积层的数量
-        for i in range(used_backbone_levels - 1, 0, -1):  # 从最后一层往前遍历
-            prev_shape = laterals[i - 1].shape[2:]  # 获取上一层的空间维度（不包括 batch 和通道）
-            laterals[i - 1] = self.galas[i-1](F.interpolate(  laterals[i], size=prev_shape, mode='nearest'  ), laterals[i - 1])
+        # build top-down path
+        used_backbone_levels = len(laterals)
+        for i in range(used_backbone_levels - 1, 0, -1):
+            prev_shape = laterals[i - 1].shape[2:]
+            laterals[i - 1] += F.interpolate(
+                laterals[i], size=prev_shape, mode='nearest'
+            )
 
-        # 对每一层的特征图进行 FPN 卷积处理
-        # outs = [self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)]
-        
-        # 返回每一层的输出特征图
-        return tuple(laterals)
+        outs = [self.fpn_convs[i](laterals[i]) for i in range(used_backbone_levels)]
+        return tuple(outs)
