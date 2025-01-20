c71e873f21682b2679f5e4d44f0a05cfd9beffbc
FriendChi <137796057+FriendChi@users.noreply.github.com>
2025-01-20 11:14:17 +0800

    Update fpn.py

--- unlanedet/model/module/neck/fpn.py
+++ unlanedet/model/module/neck/fpn.py
@@ -22,13 +22,13 @@ class eca_layer(nn.Module):
    def __init__(self, channel, k_size=3):
        super(eca_layer, self).__init__()
        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-       self.max_pool = nn.AdaptiveMaxPool2d(1)
+       # self.max_pool = nn.AdaptiveMaxPool2d(1)
        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
        self.sigmoid = nn.Sigmoid()
 
    def forward(self, x):
        # feature descriptor on the global spatial information
-       y = self.avg_pool(x)+self.max_pool(x)
+       y = self.avg_pool(x)
        
        # Two different branches of ECA module
        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
